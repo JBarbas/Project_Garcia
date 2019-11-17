18a225f894c4fc99b4ee4d7c0dc7c32404ce190f
Mario <mariofdezfdez@hotmail.com>
2019-11-17 23:25:41 +0100

    avances cameraPrueba

---
+++ camera_prueba/Assets/Scripts/InterfaceControllerGameplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceControllerGameplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        else if (Input.GetKeyUp("escape"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+
+        else if (Input.GetKeyDown("p"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (Input.GetKeyUp("p"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
--- camera_prueba/Assets/Scripts/caminarScript.cs
+++ camera_prueba/Assets/Scripts/caminarScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class caminarScript : MonoBehaviour {
 
@@ -20,5 +21,6 @@ public class caminarScript : MonoBehaviour {
             this.GetComponent<Animator>().ResetTrigger("caminar");
             this.GetComponent<Animator>().SetTrigger("parar");
         }
+
     }
 }
