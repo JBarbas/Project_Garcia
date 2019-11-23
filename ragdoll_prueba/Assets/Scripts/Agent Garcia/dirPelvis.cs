d3dc16e6bd57437513d3dfd72f19a3a7bad6b17d
JBarbas <jbarbascubero@gmail.com>
2019-11-23 16:22:10 +0100

    Planking y puertas

--- ragdoll_prueba/Assets/Scripts/Agent Garcia/dirPelvis.cs
+++ ragdoll_prueba/Assets/Scripts/Agent Garcia/dirPelvis.cs
@@ -24,6 +24,32 @@ public class dirPelvis : MonoBehaviour
     private bool moveLeft = false;
     private bool moveRight = false;
     private bool moveBack = false;
+    public bool planking = false;
+    public bool plankingMade = false;
+    public Transform plankingTarget;
+    private float targetRotation;
+
+    public Vector3 JumpVector;
+
+    public bool getJumping()
+    {
+        return jumping;
+    }
+
+    public void setJumping(bool jumping)
+    {
+        this.jumping = jumping;
+    }
+
+    public bool isPlanking()
+    {
+        return planking;
+    }
+
+    public void setPlanking(bool planking)
+    {
+        this.planking = planking;
+    }
 
     IEnumerator RotatePelvis(float targetRotation)
     {
@@ -48,52 +74,55 @@ public class dirPelvis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float targetRotation = camera.transform.eulerAngles.y;
+        if (!planking)
+        {
+            targetRotation = camera.transform.eulerAngles.y;
 
-        if (Input.GetKeyDown("w"))
-        {
-            moveFront = true;
-        }
-        else if (Input.GetKeyUp("w"))
-        {
-            moveFront = false;
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            moveLeft = true;
-        }
-        else if (Input.GetKeyUp("a"))
-        {
-            moveLeft = false;
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            moveBack = true;
-        }
-        else if (Input.GetKeyUp("s"))
-        {
-            moveBack = false;
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            moveRight = true;
-        }
-        else if (Input.GetKeyUp("d"))
-        {
-            moveRight = false;
-        }
-        if (Input.GetKeyDown("space") && !jumping)
-        {
-            jumping = true;
-            jump();
-        }
-        if (Input.GetKeyDown("left shift"))
-        {
-            running = true;
-        }
-        else if (Input.GetKeyUp("left shift"))
-        {
-            running = false;
+            if (Input.GetKeyDown("w"))
+            {
+                moveFront = true;
+            }
+            else if (Input.GetKeyUp("w"))
+            {
+                moveFront = false;
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                moveLeft = true;
+            }
+            else if (Input.GetKeyUp("a"))
+            {
+                moveLeft = false;
+            }
+            if (Input.GetKeyDown("s"))
+            {
+                moveBack = true;
+            }
+            else if (Input.GetKeyUp("s"))
+            {
+                moveBack = false;
+            }
+            if (Input.GetKeyDown("d"))
+            {
+                moveRight = true;
+            }
+            else if (Input.GetKeyUp("d"))
+            {
+                moveRight = false;
+            }
+            if (Input.GetKeyDown("space") && !jumping)
+            {
+                jumping = true;
+                jump();
+            }
+            if (Input.GetKeyDown("left shift"))
+            {
+                running = true;
+            }
+            else if (Input.GetKeyUp("left shift"))
+            {
+                running = false;
+            }
         }
 
         float direction = 0;
@@ -111,7 +140,20 @@ public class dirPelvis : MonoBehaviour
             if (moveBack) direction += -135;
         }
 
-        if (moveFront || moveLeft || moveBack || moveRight)
+        if (planking)
+        {
+            Vector3 currentPos = transform.position;
+            transform.position = origin;
+            Quaternion desiredRotation = Quaternion.Euler(-180, targetRotation - 180, 0);
+            parent.transform.rotation = Quaternion.Lerp(parent.transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            transform.position = currentPos;
+            if (!plankingMade)
+            {
+                float step = rotateSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, plankingTarget.position, step);
+            }
+        }
+        if (moveFront || moveLeft || moveBack || moveRight && !planking)
         {
             Vector3 currentPos = transform.position;
             transform.position = origin;
@@ -138,11 +180,29 @@ public class dirPelvis : MonoBehaviour
         if (collision.gameObject.layer == 11 && jumping)
         {
             jumping = false;
+            rb.velocity = Vector3.zero;
         }
     }
 
     void jump()
     {
-        rb.AddForce(new Vector3(0, jumpForce * 100, 0), ForceMode.Impulse);
+        //rb.AddForce(new Vector3(0, jumpForce * 100, 0), ForceMode.Impulse);
+        rb.AddForce(JumpVector * jumpForce * 100, ForceMode.Impulse);
+    }
+
+    public void exitPlanking()
+    {
+        Vector3 currentPos = transform.position;
+        transform.position = origin;
+        parent.transform.rotation = Quaternion.Euler(-90, parent.transform.rotation.eulerAngles.y - 180, 0);
+        transform.position = currentPos;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "planking collider")
+        {
+            //plankingMade = true;
+        }
     }
 }
---
+++ ragdoll_prueba/Assets/Scripts/InteractiveGameObjects/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : InteractiveItem
+{
+
+    public float kickForce = 50f;
+
+    override
+    public void onInteract()
+    {
+        interactionText.text = "";
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(-kickForce, 0, 0), GetComponentInParent<Transform>().position + new Vector3(0,1,0));
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Agent Garcia" && PlayerInventory.hasKey)
+        {
+            interactionText.text = text + " " + itemName;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                onInteract();
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Agent Garcia")
+        {
+            interactionText.text = "";
+        }
+    }
+}
