ef0e420296ccb3b770045698732b785461c844b7
Luis_Cheek <luis.betancourt.ortega@gmail.com>
2019-11-24 13:39:55 +0100

    Maquina de estados para equipo zero gravity y guardias

---
+++ ragdoll_prueba/Assets/Cosas de Luis/Buscar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Buscar : StateMachineBehaviour
+{
+    private NavMeshAgent _agent;
+    private GameObject NPC;
+    private float speed = 0.0f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _agent = animator.gameObject.GetComponent<NavMeshAgent>();
+        _agent.speed = speed;
+        NPC = animator.gameObject;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+}
---
+++ ragdoll_prueba/Assets/Cosas de Luis/wasdMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wasdMove : MonoBehaviour
+{
+    public float speed = 7.0f;
+    private bool dead = false;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
+
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    public void morir()
+    {
+        dead = true;
+        Debug.Log("Muerto");
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Security"))
+        {
+            if (other.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Golpear"))
+            {
+                morir();
+            }
+        }
+    }
+}
